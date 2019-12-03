2149ffe02322f2e696dd6ca7c7e063533fe57367
Korisnik <Korisnik@DESKTOP-2CRP2O3>
2019-12-03 13:01:00 +0100

    Add project files.

---
+++ SeleniumTests1/Class1.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTests1
+{
+    class Class1
+    {
+        IWebDriver driver;
+
+        [Test]
+        public void TestGoogleSearch()
+        {
+            System.Threading.Thread.Sleep(1500);
+            driver.Navigate().GoToUrl("https://wwww.google.com/");
+            System.Threading.Thread.Sleep(1000);
+            IWebElement searchField = driver.FindElement(By.Name("q"));
+            searchField.SendKeys("C# selenium tests");
+            System.Threading.Thread.Sleep(2000);
+            searchField.SendKeys(Keys.Enter);
+            System.Threading.Thread.Sleep(1000);
+        }
+
+        [SetUp]
+        public void Init()
+        {
+            driver = new FirefoxDriver();
+        }
+
+        [TearDown]
+        public void Destroy()
+        {
+            driver.Close();
+        }
+
+    }
+}
